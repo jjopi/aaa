c92269c0e13aa17e9c79cbc3be8ba74c692a7e1c
zyf <754254702@qq.com>
2020-10-22 16:13:22 +0800

    第一次提交

---
+++ src/main/java/com/pku/zyf/aaademo/AaademoApplication.java
@@ -0,0 +1,13 @@
+package com.pku.zyf.aaademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaademoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pku/zyf/aaademo/AaademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pku.zyf.aaademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
